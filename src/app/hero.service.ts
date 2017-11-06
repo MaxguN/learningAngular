93cd627b31090625af199db9a4b959d731c61235
Florent Fortat <florent.fortat@tralalere.com>
2017-11-06 18:24:12 +0100

    Up to 6. Routing - Style the app

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,4 +11,9 @@ export class HeroService {
 		// 	setTimeout(() => resolve(HEROES), 2000);
 		// });
 	}
+
+	getHero(id: number): Promise<Hero> {
+		return this.getHeroes()
+			.then(heroes => heroes.find(hero => hero.id === id));
+	}
 }
